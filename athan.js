1c82236a25892f399dcc4f070aba784393397408
Nawaf <nawaftalaat@hotmail.com>
2023-01-08 11:43:01 +0300

    Done

--- athan.js
+++ athan.js
@@ -2,25 +2,36 @@
 let parent = document.getElementsByClassName('parent')[0]
 let city = document.getElementById('city');
 let submit = document.getElementsByClassName('submit')[0]
+let day = document.getElementById('day')
+let today = document.getElementById('today')
+let country = document.getElementsByClassName('country')[0]
+
+
 submit.addEventListener('click',(e)=>{
-fetch(` http://api.aladhan.com/v1/calendarByAddress?address= ${city.value}`)
+fetch(` http://api.aladhan.com/v1/timingsByCity?city=${city.value}&country=ff `)
 .then((res)=>res.json())
 .then((res)=>{
-    let today = document.getElementById('today')
-    today.textContent = res.data[0].date.gregorian.date
+    
+    
+    country.innerHTML = city.value.replace(city.value[0],city.value[0].toUpperCase())
+    day.textContent = Object.values(res.data.date.gregorian.weekday).join('');
+    
+    today.textContent = res.data.date.gregorian.date
+    console.log(res)
     let timings = []
-    timings.push(Object.values(res.data[0].timings));
+    timings.push(Object.values(res.data.timings));
     timings = timings[0]
     timings.length = 7;
     console.log(timings)
 
-    let athan =[]
-     athan.push(Object.keys(res.data[0].timings))
+    let athan =[]    
+     athan.push(Object.keys(res.data.timings))
      athan[0].length = 7
     athan =athan[0]
     
+   
+    
     parent.innerHTML = '';
-
     for(i=0;i<timings.length;i++){
         let p = document.createElement('p')
         let h3 = document.createElement('h3')
@@ -33,9 +44,14 @@ fetch(` http://api.aladhan.com/v1/calendarByAddress?address= ${city.value}`)
         div.append(hr)
         div.append(p)
         parent.append(div)
-    
+        
     }
-    
 })
+
 e.preventDefault()
+
 })
+
+let a = 'makkah'
+
+console.log()
